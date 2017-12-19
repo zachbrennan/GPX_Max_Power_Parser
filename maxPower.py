6f17fa5db803b6c5d432896a24f8fcf69e21abbb
Zach <zab37@drexel.edu>
2017-12-19 14:37:34 -0500

    well that was easier than expected

--- maxPower.py
+++ maxPower.py
@@ -2,9 +2,21 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('testFile.gpx')
 root = tree.getroot()
 
-print(root)
+
+totalSteps = 0
+totalPower = 0
+avgPower = 0
+maxPower = 0
 
 for powerStep in root.iter("{http://www.topografix.com/GPX/1/1}power"):
-    print(powerStep.text)
+    totalPower += int(powerStep.text)
+    totalSteps += 1
+    if (int(powerStep.text) > maxPower):
+        maxPower = int(powerStep.text)
+
+avgPower = totalPower / totalSteps
+print("Sum of power numbers:" , totalPower)
+print("Average Power:" , avgPower)
+print("Max 1s Power:" , maxPower)
 
 
