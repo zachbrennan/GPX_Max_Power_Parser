19f58139b5ee486d2860a11c692510d7d0bdb7bb
Zach <zab37@drexel.edu>
2017-12-19 22:09:40 -0500

    generalized finding max power over an interval

--- maxPower.py
+++ maxPower.py
@@ -24,5 +24,21 @@ def maxThreeSecond(root):
 
     print("Max 3s Power:", maxThree)
     
+def maxIntPow(root, interval):
+    maxInt = 0
+    steps = list(root.iter("{http://www.topografix.com/GPX/1/1}power"))
+    
+    for i in range(0,len(steps) - (interval - 1)):
+        tempInt = 0
+        for j in range(0, interval):
+            tempInt += int(steps[i+j].text)
+        tempInt /= interval
+        
+        if (tempInt > maxInt):
+            maxInt = tempInt
+
+    print("Max " + str(interval) + "s Power:" + str(maxInt))
+
 maxOneSecond(root)
 maxThreeSecond(root)
+maxIntPow(root, 5)
