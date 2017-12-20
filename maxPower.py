60c2ddd8e462230d3db221c902cb6b499c21a1e4
Zach <zab37@drexel.edu>
2017-12-19 21:59:37 -0500

    broke into functions, added 3s power

--- maxPower.py
+++ maxPower.py
@@ -1,22 +1,28 @@
 import xml.etree.ElementTree as ET
+
 tree = ET.parse('testFile.gpx')
 root = tree.getroot()
 
+def maxOneSecond(root):
+    maxPower = 0
 
-totalSteps = 0
-totalPower = 0
-avgPower = 0
-maxPower = 0
+    for powerStep in root.iter("{http://www.topografix.com/GPX/1/1}power"):
+        if (int(powerStep.text) > maxPower):
+            maxPower = int(powerStep.text)
 
-for powerStep in root.iter("{http://www.topografix.com/GPX/1/1}power"):
-    totalPower += int(powerStep.text)
-    totalSteps += 1
-    if (int(powerStep.text) > maxPower):
-        maxPower = int(powerStep.text)
+    print("Max 1s Power:" , maxPower)
 
-avgPower = totalPower / totalSteps
-print("Sum of power numbers:" , totalPower)
-print("Average Power:" , avgPower)
-print("Max 1s Power:" , maxPower)
+def maxThreeSecond(root):
+    
+    maxThree = 0
+    steps = list(root.iter("{http://www.topografix.com/GPX/1/1}power"))
 
+    for i in range(0,len(steps) - 2):
+        tempThree = ((int(steps[i].text) + int(steps[i+1].text) + int(steps[i+2].text)) / 3)
+        if (tempThree > maxThree):
+            maxThree = tempThree
 
+    print("Max 3s Power:", maxThree)
+    
+maxOneSecond(root)
+maxThreeSecond(root)
