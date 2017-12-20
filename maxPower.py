daeef09cb23ef0dcd40b6e758f0b6823b1389737
Zach <zab37@drexel.edu>
2017-12-19 22:17:53 -0500

    removed extraneous code

--- maxPower.py
+++ maxPower.py
@@ -1,32 +1,12 @@
 import xml.etree.ElementTree as ET
 
 tree = ET.parse('testFile.gpx')
+powerTag = "{http://www.topografix.com/GPX/1/1}power"
 root = tree.getroot()
+steps = list(root.iter(powerTag))    
 
-def maxOneSecond(root):
-    maxPower = 0
-
-    for powerStep in root.iter("{http://www.topografix.com/GPX/1/1}power"):
-        if (int(powerStep.text) > maxPower):
-            maxPower = int(powerStep.text)
-
-    print("Max 1s Power:" , maxPower)
-
-def maxThreeSecond(root):
-    
-    maxThree = 0
-    steps = list(root.iter("{http://www.topografix.com/GPX/1/1}power"))
-
-    for i in range(0,len(steps) - 2):
-        tempThree = ((int(steps[i].text) + int(steps[i+1].text) + int(steps[i+2].text)) / 3)
-        if (tempThree > maxThree):
-            maxThree = tempThree
-
-    print("Max 3s Power:", maxThree)
-    
-def maxIntPow(root, interval):
+def maxIntPow(steps, interval):
     maxInt = 0
-    steps = list(root.iter("{http://www.topografix.com/GPX/1/1}power"))
     
     for i in range(0,len(steps) - (interval - 1)):
         tempInt = 0
@@ -37,8 +17,10 @@ def maxIntPow(root, interval):
         if (tempInt > maxInt):
             maxInt = tempInt
 
-    print("Max " + str(interval) + "s Power:" + str(maxInt))
+    return maxInt
 
-maxOneSecond(root)
-maxThreeSecond(root)
-maxIntPow(root, 5)
+
+for i in range(1,len(steps)):
+    print(str(i) + "s max:" , maxIntPow(steps, i))
+
+
