77a62a5db3288e7572cb7c0dd8863cd031f4b957
Zach <zab37@drexel.edu>
2017-12-21 12:19:54 -0500

    split into separate files

---
+++ main.py
@@ -0,0 +1,35 @@
+from maxPower import maxIntPow
+import xml.etree.ElementTree as ET
+import plotly
+from plotly.graph_objs import Scatter, Layout
+import time
+import sys
+
+t = time.time()
+
+interval = 120
+if (len(sys.argv) > 1):
+    if (int(sys.argv[1] > 0)):
+        interval = int(sys.argv[1])
+
+tree = ET.parse('testFile.gpx')
+powerTag = "{http://www.topografix.com/GPX/1/1}power"
+root = tree.getroot()
+steps = list(root.iter(powerTag))    
+
+data = list()
+
+for i in range(1,interval + 2): 
+    t2 = time.time()
+    maxInt = maxIntPow(steps, i)
+    data.append(maxInt)
+    #print("Loop time:", time.time() - t2)
+
+plotly.offline.plot({
+    "data": [Scatter(x=range(1,len(data)), y=data)],
+    "layout": Layout(title="Power Curve")
+})
+
+elapsed = time.time() - t
+print("Elapsed time:" + str(elapsed))
+
--- maxPower.py
+++ maxPower.py
@@ -4,30 +4,6 @@ from plotly.graph_objs import Scatter, Layout
 import time
 import sys
 
-t = time.time()
-
-interval = 120
-if (len(sys.argv) > 1):
-    interval = int(sys.argv[1])
-
-tree = ET.parse('testFile.gpx')
-powerTag = "{http://www.topografix.com/GPX/1/1}power"
-root = tree.getroot()
-steps = list(root.iter(powerTag))    
-
-#def maxIntPow(steps, interval):
-#    maxInt = 0
-#    
-#    for i in range(0,len(steps) - (interval - 1)):
-#        tempInt = 0
-#        for j in range(0, interval):
-#            tempInt += int(steps[i+j].text)
-#        tempInt /= interval
-#       
-#        if (tempInt > maxInt):
-#            maxInt = tempInt
-#
-#    return maxInt
 
 def maxIntPow(steps, interval):
     maxInt = 0
@@ -48,18 +24,3 @@ def maxIntPow(steps, interval):
 
     return maxInt / interval
 
-data = list()
-#for i in range(1,len(steps)):
-for i in range(1,interval + 2): 
-    t2 = time.time()
-    maxInt = maxIntPow(steps, i)
-    data.append(maxInt)
-    #print("Loop time:", time.time() - t2)
-
-plotly.offline.plot({
-    "data": [Scatter(x=range(1,len(data)), y=data)],
-    "layout": Layout(title="Power Curve")
-})
-
-elapsed = time.time() - t
-print("Elapsed time:" + str(elapsed))
