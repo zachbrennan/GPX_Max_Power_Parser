424d6239cb3a555dd507b23ed80d1b2b2eaccf89
Zach <zab37@drexel.edu>
2017-12-20 21:42:07 -0500

    added command line arg for how far to go, in seconds

--- maxPower.py
+++ maxPower.py
@@ -2,29 +2,34 @@ import xml.etree.ElementTree as ET
 import plotly
 from plotly.graph_objs import Scatter, Layout
 import time
+import sys
 
 t = time.time()
+
+interval = 120
+if (len(sys.argv) > 1):
+    interval = int(sys.argv[1])
 
 tree = ET.parse('testFile.gpx')
 powerTag = "{http://www.topografix.com/GPX/1/1}power"
 root = tree.getroot()
 steps = list(root.iter(powerTag))    
 
+#def maxIntPow(steps, interval):
+#    maxInt = 0
+#    
+#    for i in range(0,len(steps) - (interval - 1)):
+#        tempInt = 0
+#        for j in range(0, interval):
+#            tempInt += int(steps[i+j].text)
+#        tempInt /= interval
+#       
+#        if (tempInt > maxInt):
+#            maxInt = tempInt
+#
+#    return maxInt
+
 def maxIntPow(steps, interval):
-    maxInt = 0
-    
-    for i in range(0,len(steps) - (interval - 1)):
-        tempInt = 0
-        for j in range(0, interval):
-            tempInt += int(steps[i+j].text)
-        tempInt /= interval
-        
-        if (tempInt > maxInt):
-            maxInt = tempInt
-
-    return maxInt
-
-def maxIntPow2(steps, interval):
     maxInt = 0
     tempInt = 0
 
@@ -39,20 +44,20 @@ def maxIntPow2(steps, interval):
         if (tempInt > maxInt):
             maxInt = tempInt
     
-    print( str(interval) + "s power:" + str(maxInt / interval))
+    #print( str(interval) + "s power:" + str(maxInt / interval))
 
     return maxInt / interval
 
 data = list()
 #for i in range(1,len(steps)):
-for i in range(1,31): 
+for i in range(1,interval + 2): 
     t2 = time.time()
-    maxInt = maxIntPow2(steps, i)
+    maxInt = maxIntPow(steps, i)
     data.append(maxInt)
-    print("Loop time:", time.time() - t2)
+    #print("Loop time:", time.time() - t2)
 
 plotly.offline.plot({
-    "data": [Scatter(x=range(1,31), y=data)],
+    "data": [Scatter(x=range(1,len(data)), y=data)],
     "layout": Layout(title="Power Curve")
 })
 
