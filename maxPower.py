a1cf3e435eae36ee6ca557c652b9ee33c4010245
Zach <zab37@drexel.edu>
2017-12-20 21:31:56 -0500

    changed algorithm, vastly improved runtime

--- maxPower.py
+++ maxPower.py
@@ -24,17 +24,35 @@ def maxIntPow(steps, interval):
 
     return maxInt
 
+def maxIntPow2(steps, interval):
+    maxInt = 0
+    tempInt = 0
+
+    for k in range(0, interval):
+        tempInt += int(steps[k].text)
+
+    for i in range(interval,len(steps) - (interval - 1)):
+
+        tempInt -= int(steps[i-interval].text)
+        tempInt += int(steps[i].text)
+
+        if (tempInt > maxInt):
+            maxInt = tempInt
+    
+    print( str(interval) + "s power:" + str(maxInt / interval))
+
+    return maxInt / interval
 
 data = list()
 #for i in range(1,len(steps)):
-for i in range(1,61): 
+for i in range(1,31): 
     t2 = time.time()
-    maxInt = maxIntPow(steps, i)
+    maxInt = maxIntPow2(steps, i)
     data.append(maxInt)
     print("Loop time:", time.time() - t2)
 
 plotly.offline.plot({
-    "data": [Scatter(x=range(1,61), y=data)],
+    "data": [Scatter(x=range(1,31), y=data)],
     "layout": Layout(title="Power Curve")
 })
 
